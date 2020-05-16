8d3500db6de6d8029e8e6109cf6dcc21ca9a4d17
Doug Beney <git@dougie.io>
2020-05-16 13:43:50 -0400

    first

---
+++ defaults.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chsh --shell /usr/bin/fish
+
+git clone git@github.com:DougBeney/Emacs-Config-v2.git ~/.emacs.d
+git clone git@github.com:DougBeney/Fish-Config.git ~/.config/fish
---
+++ packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo eopkg it \
+emacs \
+fish \
+inkscape \
+gimp \
+thunderbird \
+wine \
+font-roboto-ttf \
+font-ubuntu-ttf	\
+font-manager \
+git \
+python3 \
+tree
